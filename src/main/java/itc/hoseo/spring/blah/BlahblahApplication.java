7091dc055893bc2d2b8e229b4cc6e731372b732c
SOOBINEEY\Soobineey <tnehfdl2011@gmail.com>
2019-05-18 12:19:56 +0900

    f commit

---
+++ src/main/java/itc/hoseo/spring/blah/BlahblahApplication.java
@@ -0,0 +1,13 @@
+package itc.hoseo.spring.blah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlahblahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlahblahApplication.class, args);
+	}
+
+}
