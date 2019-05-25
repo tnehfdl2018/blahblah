99bee50433d92f5532ddc51fecd19fb6e1611815
이다일 <dilliyo@naver.com>
2019-05-26 03:35:40 +0900

    190526_0335

--- src/main/java/itc/hoseo/spring/blah/MainController.java
+++ src/main/java/itc/hoseo/spring/blah/MainController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.collect.ImmutableList;
 
 import itc.hoseo.spring.blah.team.Team;
 import itc.hoseo.spring.blah.team.TeamMapper;
@@ -15,10 +18,16 @@ public class MainController {
 
 	@Autowired
 	private TeamMapper teamMapper;
-	
-	@ResponseBody
-	@RequestMapping("/teams")
-	public List<Team> getTeams() {
-		return teamMapper.getTeams();
+
+	@RequestMapping("/")
+	public String getTeams(ModelMap m) {
+		
+		
+		List<Team> teams = ImmutableList.of(
+				Team.builder().teamNameEn("Kiaaaaaaaaaaaaa").teamNameKr("키아 주모").build(),
+				Team.builder().teamNameEn("Hyunnnnnnnnn").teamNameKr("횬다이").build(),
+				Team.builder().teamNameEn("skkkkkkkkkk").teamNameKr("스크으").build());
+		m.put("teams", teams);
+		return "index";
 	}
 }
